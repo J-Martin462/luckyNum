45c352d52529060f71a3df5b0e47661ba0c3d5f5
Jesse Martin del Campo <904225@H2WHK12TQ6NW.tld>
2022-09-07 13:03:40 -0500

    generated random list and other and guess list

--- luckynum.py
+++ luckynum.py
@@ -2,10 +2,27 @@ import random
 
 
 
-numList = {random.randint(1,70)}
+numList = [random.randint(1,70)]
 print (numList)
 
 i=1
 while i != 70:
-    numList.push(random.randint(1,70))
-    print (numList)
+    numList.append(random.randint(1,70))
+    i+=1
+
+guessLst = []
+
+i=1 
+
+while i != 6:
+    guessLst.append(numList[random.randint(0,69)])
+    i+=1
+    print(guessLst)
+
+guess = "we"
+while guess.isdigit() == False:
+    guess = input('Enter a number 1 - 70')
+int(guess)
+
+
+
