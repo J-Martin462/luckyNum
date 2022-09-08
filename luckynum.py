9f441bf22d5d78118b5d84d5cbe1393828a08c2f
Jesse Martin del Campo <904225@H2WHK12TQ6NW.tld>
2022-09-08 13:03:47 -0500

    conditionals added

--- luckynum.py
+++ luckynum.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 numList = [random.randint(1,70)]
 print (numList)
 
@@ -10,19 +8,34 @@ while i != 70:
     numList.append(random.randint(1,70))
     i+=1
 
+keyLst = []
+
 guessLst = []
 
 i=1 
 
+cont = True
+
 while i != 6:
-    guessLst.append(numList[random.randint(0,69)])
+    keyLst.append(numList[random.randint(0,69)])
     i+=1
+    print(keyLst)
+while cont == True:
+    guess = 71
+    while str(guess).isdigit() == False and guess > 70:
+        guess = input('Enter a number 1 - 70: ')
+        if guess > 70:
+            print('Enter a number less than or equal to 70')
+        elif guess.isdigit() == False:
+            print('Please only enter a number')
+    int(guess)
+
+    guessLst.append(guess)
+
     print(guessLst)
 
-guess = "we"
-while guess.isdigit() == False:
-    guess = input('Enter a number 1 - 70')
-int(guess)
+    if len(guessLst) == 5:
+        cont == False
 
 
 
