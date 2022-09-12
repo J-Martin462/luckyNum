ba551236b96126a086d8988655b3c3f77ce93c08
Jesse Martin del Campo <904225@H2WHK12TQ6NW.tld>
2022-09-12 13:05:18 -0500

    Figured out sets just setname.add(term)

--- luckynum.py
+++ luckynum.py
@@ -1,5 +1,6 @@
 import random
 
+# Ramdom list gen
 numList = [random.randint(1,70)]
 print (numList)
 
@@ -8,6 +9,7 @@ while i != 70:
     numList.append(random.randint(1,70))
     i+=1
 
+#var and Lst setup
 keyLst = []
 
 guessLst = []
@@ -16,20 +18,29 @@ i=1
 
 cont = True
 
+guess = 71
+
+setLst = set(numList)
+
+#creation of the guessing list
 while i != 6:
-    keyLst.append(numList[random.randint(0,69)])
+    keyLst.append(setLst{1})
     i+=1
     print(keyLst)
+
+#user input checkpoints
 while cont == True:
-    guess = 71
-    while str(guess).isdigit() == False and guess > 70:
+    int(guess)
+    while str(guess).isdigit() == False:
+        print('Please only enter a number.')
         guess = input('Enter a number 1 - 70: ')
+    while str(guess).isdigit() == False or int(guess) > 70:
         if guess > 70:
             print('Enter a number less than or equal to 70')
+            guess = input('Enter a number 1 - 70: ')
         elif guess.isdigit() == False:
             print('Please only enter a number')
-    int(guess)
-
+            guess = input('Enter a number 1 - 70: ')
     guessLst.append(guess)
 
     print(guessLst)
