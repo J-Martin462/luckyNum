c7fb191dfcd0e8d6fdc42938ae17e55dc25c3bdd
Jesse Martin del Campo <904225@H2WHK12TQ6NW.tld>
2022-09-13 13:05:34 -0500

    Figured out sets just setname.add(term)

--- luckynum.py
+++ luckynum.py
@@ -1,12 +1,12 @@
 import random
 
 # Ramdom list gen
-numList = [random.randint(1,70)]
+numList = set()
 print (numList)
 
 i=1
 while i != 70:
-    numList.append(random.randint(1,70))
+    numList.add(random.randint(1, 70))
     i+=1
 
 #var and Lst setup
@@ -19,34 +19,36 @@ i=1
 cont = True
 
 guess = 71
-
-setLst = set(numList)
-
+print(numList)
 #creation of the guessing list
 while i != 6:
-    keyLst.append(setLst{1})
+    keyLst.append(numList.pop())
     i+=1
     print(keyLst)
 
-#user input checkpoints
-while cont == True:
-    int(guess)
-    while str(guess).isdigit() == False:
-        print('Please only enter a number.')
-        guess = input('Enter a number 1 - 70: ')
-    while str(guess).isdigit() == False or int(guess) > 70:
-        if guess > 70:
-            print('Enter a number less than or equal to 70')
-            guess = input('Enter a number 1 - 70: ')
-        elif guess.isdigit() == False:
-            print('Please only enter a number')
-            guess = input('Enter a number 1 - 70: ')
-    guessLst.append(guess)
+# #user input checkpoints
+# while cont == True:
+#     int(guess)
+#     while str(guess).isdigit() == False:
+#         print('Please only enter a number.')
+#         guess = input('Enter a number 1 - 70: ')
+#     while str(guess).isdigit() == False or int(guess) > 70:
+#         if str(guess).isdigit() == False:
+#             print('Enter a number less than or equal to 70')
+#             guess = input('Enter a number 1 - 70: ')
+#         elif int(guess) > 70:
+#             print('Please only enter a number')
+#             guess = input('Enter a number 1 - 70: ')
+#     guessLst.append(guess)
 
-    print(guessLst)
+#     print(guessLst)
 
-    if len(guessLst) == 5:
-        cont == False
+#     if len(guessLst) == 5:
+#         cont == False
 
 
+input("I need to learn how not to leave my computer unlocked.")
 
+input("How long will this go?")
+
+input("Did something else get deleted? Ahhh!!! A mistery.")
